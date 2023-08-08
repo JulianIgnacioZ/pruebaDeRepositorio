7a520635df1f66044f05f554b9fa0d89ed575be6
Julian Zampini <julianzampini@gmail.com>
2023-08-08 14:24:11 -0300

    purebaDeLaPrueba

--- arreglos.js
+++ arreglos.js
@@ -1,10 +1,19 @@
 function descripcionArreglo(x) {
     if (x.length <= 5) {
-        return "Es un arreglo chico"
+        return "Es un arreglo chico";
     } else {
-        return "Es un arreglo grande"
+        return "Es un arreglo grande";
     }
 }
 
-console.log(descripcionArreglo([1, 2, 3, 4,]))
-console.log(descripcionArreglo([1, 2, 3, 4, 5, 6]))
+console.log(descripcionArreglo([1, 2, 3, 4,]));
+console.log(descripcionArreglo([1, 2, 3, 4, 5, 6]));
+
+const array1 = ["Julian"];
+function arregloNombres(array1) {
+    if (array1) {
+        return "Julian se enceuntra en el grupo";
+    } else {
+        return "Julian no se encuentra en el grupo";
+    }
+}
